48bc5c132e1ec484e07655be45b648dc88581221
CIBERCOP20001 <cibercop20001@hotmail.com>
2017-10-18 18:31:45 -0500

    LoginValidation

---
+++ ShowUsers.Data/AccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShowUsers.Interface;
+using ShowUsers.Model.Models;
+
+namespace ShowUsers.Data
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public bool ValidateLogin(Login login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ShowUsers.Interface/IAccountRepository.cs
+++ ShowUsers.Interface/IAccountRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShowUsers.Model;
 
 namespace ShowUsers.Interface
 {
-    public class Class1
+    public interface IAccountRepository
     {
+        bool ValidateLogin(ShowUsers.Model.Models.Login login);
     }
 }
